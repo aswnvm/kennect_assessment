bbc12e203e2ef6b182b88b2ae69202fe8e137313
aswnvm <113505575+aswnvm@users.noreply.github.com>
2022-12-09 12:47:07 +0530

    Update Qwes3.js

--- Qwes3.js
+++ Qwes3.js
@@ -1,27 +1,31 @@
-isPrime = (num) => {	
-    let f = true;	
-	num = document.myform.num.value;
-    num = parseInt(num)
-   let sqrtnum = Math.floor(Math.sqrt(num));
-   let prime = num;
-   
-   for(let i = 2; i < sqrtnum + 1; i++){
-
-       if(num % i == 0){
-           
-           f = false;
-           break;
-       }
-   }
-   alert(prime+ "Is prime");
-   return prime;
+function isPrime(){
+  var n, i, flag = true;
+  n = document.myform.n.value;
+  n = parseInt(n);
+  for (i = 2;  i < n; i++){
+    if (n % i == 0) {
+      flag = false;
+      break;
+    }
+  }
+  if(flag == true){
+    find = nextPrime(n);
+  }
+  else
+    alert(n + " is not a prime number");
 }
-nextPrime = (num) => {
 
-   while(!isPrime(++num)){
-   }
-
-   return num;
-   alert(num+ "Is prime");
+function nextPrime(n) {
+  var j, flag, next, diff = true;
+  for (next = n + 1; ; next++) {
+    for(j = 2; j < next; j++){
+      if(next % j == 0){
+        flag = false;
+        break;
+      }
+    }  
+  }
+  diff = next - n;
+  if(flag == true)
+    alert(next + ," is the next closest prime number of the given prime number " + n + " and " + diff + " is the difference between them.")
 }
-console.log(isPrime(num));
